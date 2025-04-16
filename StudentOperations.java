e0a709aa4faabbe68f7549e2de427a38b6fc8c3f
Anusri Kadam <anusrikadam@gmail.com>
2025-04-16 10:18:51 +0530

    Update StudentOperations.java

--- StudentOperations.java
+++ StudentOperations.java
@@ -1,2 +1,7 @@
 import java.sql.*;
 import java.util.Scanner;
+
+public class StudentOperations {
+    private static final String URL = "jdbc:mysql://localhost:3306/db_student";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123456";
